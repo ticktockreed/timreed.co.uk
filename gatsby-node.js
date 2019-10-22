b368cea004b2045124463a2285f1a8da7f4a67a1
Tim Reed <tim@alloftheabove.co.uk>
2019-10-22 17:19:07 +0100

    remove gatsby node config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,31 @@
-const path = require("path");
+// const path = require("path");
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
 
-  const pages = await graphql(`
-    {
-      allPrismicLandingPage {
-        edges {
-          node {
-            id
-            uid
-          }
-        }
-      }
-    }
-  `);
+//   const pages = await graphql(`
+//     {
+//       allPrismicLandingPage {
+//         edges {
+//           node {
+//             id
+//             uid
+//           }
+//         }
+//       }
+//     }
+//   `);
 
-  const template = path.resolve("src/templates/landingPage.jsx");
+//   const template = path.resolve("src/templates/landingPage.jsx");
 
-  pages.data.allPrismicLandingPage.edges.forEach(edge => {
-    console.log(edge);
-    createPage({
-      path: `/${edge.node.uid}`,
-      component: template,
-      context: {
-        uid: edge.node.uid
-      }
-    });
-  });
-};
+//   pages.data.allPrismicLandingPage.edges.forEach(edge => {
+//     console.log(edge);
+//     createPage({
+//       path: `/${edge.node.uid}`,
+//       component: template,
+//       context: {
+//         uid: edge.node.uid
+//       }
+//     });
+//   });
+// };
