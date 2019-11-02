cf6607b195c7fb9494ad179270f01e025e42f6f2
Tim Reed <tim@alloftheabove.co.uk>
2019-11-02 07:23:23 +0000

    nav icon animation

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,36 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import { graphql } from "gatsby";
 
-// import AnimationExample from "../components/AnimationExample";
-// import ChartExample from "../components/ChartExample";
-// import AnimationExample from "../components/AnimationExample";
-
-let chartData = [];
-
-function mockData(variance) {
-  let gaussian = x => {
-    let mean = 0;
-    return (
-      (1 / Math.sqrt(2 * Math.PI * variance)) *
-      Math.exp((-(x - mean) * (x - mean)) / (2 * variance))
-    );
-  };
-
-  chartData = [];
-  for (let i = -5; i < 5; i += 0.1) {
-    chartData.push(gaussian(i));
-  }
-}
-
-const IndexPage = ({ data: { prismicLandingPage } }) => {
-  const { data } = prismicLandingPage;
-  const [variance] = useState(0.2);
-  // const [paused, pauseAnimation] = useState(false);
-  useEffect(() => {
-    mockData(variance);
-  }, [variance]);
+const IndexPage = ({ data: { prismicWork } }) => {
+  const { data } = prismicWork;
 
   return (
     <Layout>
@@ -47,7 +21,23 @@ const IndexPage = ({ data: { prismicLandingPage } }) => {
       <div className="row justify-content-center">
         <div className="col-10 col-lg-6">
           <div className="richtext">
-            <div dangerouslySetInnerHTML={{ __html: data.page_content.html }} />
+            {console.log("data.body[0].items)", data.body[0].items)}
+
+            {data.body[0].items.map(({ work_item }) => {
+              if (!work_item) {
+                return false;
+              }
+
+              const { data, uid } = work_item.document[0];
+              return (
+                <div className="work-item" key={`work-item_${uid}`}>
+                  {/* work item */}
+                  {data.title.text}
+                  <img src={data.main_image.url} alt={data.main_image.alt} />
+                </div>
+              );
+            })}
+            {/* <div dangerouslySetInnerHTML={{ __html: data.page_content.html }} /> */}
           </div>
         </div>
       </div>
@@ -59,20 +49,30 @@ export default IndexPage;
 
 export const pageQuery = graphql`
   query {
-    prismicLandingPage(uid: {}, data: {}) {
-      prismicId
+    prismicWork {
       data {
-        page_title {
-          text
-        }
-        meta_title {
-          text
-        }
-        page_intro {
-          text
-        }
-        page_content {
-          html
+        body {
+          slice_type
+          items {
+            work_item {
+              document {
+                data {
+                  main_image {
+                    url
+                    alt
+                    dimensions {
+                      height
+                      width
+                    }
+                  }
+                  title {
+                    text
+                  }
+                }
+                uid
+              }
+            }
+          }
         }
       }
     }
