13b903e80e52ce8066e38cf35413af595bb02f39
Tim Reed <tim@alloftheabove.co.uk>
2019-11-02 10:54:06 +0000

    add perspective adjustment on mouseover

--- src/components/WorkItem.js
+++ src/components/WorkItem.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 
 const WorkItem = ({ data, uid }) => {
   const [mousePointer, setMousePointer] = useState({
@@ -9,32 +9,39 @@ const WorkItem = ({ data, uid }) => {
     y: 0
   });
   const workItemWrapper = useRef(null);
+  const inner = useRef(null);
 
-  useEffect(() => {
-    console.log("onload");
-  }, []);
-
-  const handleMouseMove = (evt, workItemWrapper) => {
+  const handleMouseMove = (evt, workItemWrapper, inner) => {
     const _x = workItemWrapper.offsetLeft + workItemWrapper.offsetWidth / 2;
     const _y = workItemWrapper.offsetTop + workItemWrapper.offsetHeight / 2;
+    const x = evt.clientX - _x;
+    const y = evt.clientY - _y;
+
+    const rotateX = (x / (inner.offsetWidth / 4)).toFixed();
+    const rotateY = (y / (inner.offsetHeight / 4)).toFixed();
+
+    console.log(rotateX, rotateY);
+
     setMousePointer({
       active: true,
       _x,
       _y,
-      x: evt.clientX - _x,
-      y: evt.clientY - _y
+      x,
+      y,
+      rotateX,
+      rotateY
     });
   };
 
-  const handleMouseOut = evt => {
-    const _x = workItemWrapper.offsetLeft + workItemWrapper.offsetWidth / 2;
-    const _y = workItemWrapper.offsetTop + workItemWrapper.offsetHeight / 2;
+  const handleMouseOut = () => {
     setMousePointer({
-      active: false,
-      _x,
-      _y,
-      x: evt.clientX - _x,
-      y: evt.clientY - _y
+      active: true,
+      _x: 0,
+      _y: 0,
+      x: 0,
+      y: 0,
+      rotateX: 0,
+      rotateY: 0
     });
   };
 
@@ -44,31 +51,38 @@ const WorkItem = ({ data, uid }) => {
         className="work-item"
         key={`work-item_${uid}`}
         onMouseLeave={handleMouseOut}
-        onMouseMove={evt => handleMouseMove(evt, workItemWrapper.current)}
+        onMouseMove={evt =>
+          handleMouseMove(evt, workItemWrapper.current, inner.current)
+        }
         ref={workItemWrapper}
       >
         <div
+          className="work-item__mouse-position"
           style={{
-            position: "fixed",
-            display: mousePointer.active ? "block" : "none",
-            right: 0,
-            top: 0,
-            backgroundColor: "rgba(0,0,0, 1)",
-            padding: 10
+            display: mousePointer.active ? "block" : "none"
           }}
         >
           X: {mousePointer.x}, Y: {mousePointer.y}
         </div>
         {/* work item */}
         {data.title.text}
-        <img src={data.main_image.url} alt={data.main_image.alt} />
+
+        <div
+          className="work-item__image-wrapper"
+          ref={inner}
+          style={{
+            transform: `rotateX(${mousePointer.rotateX}deg) rotateY(${mousePointer.rotateY}deg)`
+          }}
+        >
+          <img src={data.main_image.url} alt={data.main_image.alt} />
+        </div>
       </div>
       <div
+        className="work-item__center-dot"
         style={{
-          position: "absolute",
+          display: mousePointer.active ? "block" : "none",
           top: mousePointer._y,
-          left: mousePointer._x,
-          border: "solid 5px red"
+          left: mousePointer._x
         }}
       ></div>
     </>
