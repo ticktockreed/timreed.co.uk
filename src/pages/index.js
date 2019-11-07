6f5139956acb1037519b757525532459bad1f74d
Tim Reed <tim@alloftheabove.co.uk>
2019-11-07 14:24:36 +0000

    correctly positioned mask

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,31 +9,7 @@ import AnimationExample from "../components/AnimationExample";
 import Logomask from "../images/Logomask.svg";
 import ImageGrid from "../components/ImageGrid";
 
-let chartData = [];
-
-function mockData(variance) {
-  let gaussian = x => {
-    let mean = 0;
-    return (
-      (1 / Math.sqrt(2 * Math.PI * variance)) *
-      Math.exp((-(x - mean) * (x - mean)) / (2 * variance))
-    );
-  };
-
-  chartData = [];
-  for (let i = -5; i < 5; i += 0.1) {
-    chartData.push(gaussian(i));
-  }
-}
-
 const IndexPage = ({ data: { prismicLandingPage } }) => {
-  const { data } = prismicLandingPage;
-  const [variance] = useState(0.2);
-  // const [paused, pauseAnimation] = useState(false);
-  useEffect(() => {
-    mockData(variance);
-  }, [variance]);
-
   return (
     <Layout>
       <SEO
@@ -46,30 +22,25 @@ const IndexPage = ({ data: { prismicLandingPage } }) => {
           `gatsby`
         ]}
       />
-      <div className="container"></div>
+
       <div className="container">
-        <div className="hero">
-          <div className="row justify-content-lg-center text-right">
-            <div className="col-9 offset-2 offset-lg-0 col-lg-10">
-              <div className="logomask__wrapper">
-                <div className="logomask__tilt">
-                  <AnimationExample
-                    name="logo-anim"
-                    className="logo-anim"
-                    background="#111"
-                    pause={false}
-                  />
-                  <Logomask
-                    className="logomask"
-                    preserveAspectRatio="xMinYMin slice"
-                  ></Logomask>
-                </div>
-              </div>
+        <div className="row justify-content-lg-center align-items-center hero">
+          <div className="logomask__wrapper">
+            <div className="logomask__tilt">
+              <AnimationExample
+                name="logo-anim"
+                className="logo-anim"
+                background="#111"
+                pause={false}
+              />
+              <Logomask
+                className="logomask"
+                preserveAspectRatio="xMinYMin slice"
+              ></Logomask>
             </div>
           </div>
-
-          <div className="row justify-content-lg-center">
-            <div className="col-9 offset-2 offset-lg-0 col-lg-8">
+          <div className="col-9 offset-2 offset-lg-0 col-lg-8">
+            <div className="hero__content">
               <div className="richtext">
                 <h2>
                   Welcome to the portfolio of <br />
@@ -79,16 +50,6 @@ const IndexPage = ({ data: { prismicLandingPage } }) => {
             </div>
           </div>
         </div>
-
-        {/* <div className="row justify-content-lg-center">
-          <div className="col-9 offset-2 offset-lg-0 col-lg-8 ">
-            {data.body.map((slice, i) => {
-              if (slice.slice_type === "image_grid") {
-                return <ImageGrid slice={slice} idx={i}></ImageGrid>;
-              }
-            })}
-          </div>
-        </div> */}
       </div>
     </Layout>
   );
@@ -135,3 +96,15 @@ export const pageQuery = graphql`
     }
   }
 `;
+
+// { CLIENT GRID
+//   /* <div className="row justify-content-lg-center">
+//   <div className="col-9 offset-2 offset-lg-0 col-lg-8 ">
+//     {data.body.map((slice, i) => {
+//       if (slice.slice_type === "image_grid") {
+//         return <ImageGrid slice={slice} idx={i}></ImageGrid>;
+//       }
+//     })}
+//   </div>
+// </div> */
+// }
