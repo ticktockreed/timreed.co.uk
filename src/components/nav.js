ea47e873ec1fe412bef15af1c803a3437c553d9e
Tim Reed <tim@alloftheabove.co.uk>
2019-11-07 18:45:16 +0000

    elastic height nav items

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,36 +3,34 @@ import PropTypes from "prop-types";
 import React from "react";
 
 const Header = () => (
-  <div className="site-navigation-wrapper">
-    <nav className="site-navigation">
-      <div className="site-navigation-inner">
-        <Link
-          to={"/about"}
-          activeClassName="site-navigation__text--active"
-          className="site-navigation__text"
-          partiallyActive={true}
-        >
-          About
-        </Link>
-        <Link
-          to={"/skills"}
-          activeClassName="site-navigation__text--active"
-          className="site-navigation__text"
-          partiallyActive={true}
-        >
-          Skills
-        </Link>
-        <Link
-          to={"/work"}
-          activeClassName="site-navigation__text--active"
-          className="site-navigation__text"
-          partiallyActive={true}
-        >
-          Work
-        </Link>
-      </div>
-    </nav>
-  </div>
+  <nav className="site-navigation">
+    <div className="site-navigation-inner">
+      <Link
+        to={"/about"}
+        activeClassName="site-navigation__text--active"
+        className="site-navigation__text"
+        partiallyActive={true}
+      >
+        About
+      </Link>
+      <Link
+        to={"/skills"}
+        activeClassName="site-navigation__text--active"
+        className="site-navigation__text"
+        partiallyActive={true}
+      >
+        Skills
+      </Link>
+      <Link
+        to={"/work"}
+        activeClassName="site-navigation__text--active"
+        className="site-navigation__text"
+        partiallyActive={true}
+      >
+        Work
+      </Link>
+    </div>
+  </nav>
 );
 
 Header.propTypes = {
