858ef18f1575bc7cd17714c372bf4b78ef875bc1
Tim Reed <tim@alloftheabove.co.uk>
2019-11-11 21:15:51 +0000

    style worklist

--- src/components/WorkList.js
+++ src/components/WorkList.js
@@ -3,15 +3,19 @@ import WorkItem from "./WorkItem";
 
 const WorkList = ({ items }) => {
   return (
-    <div className="row justify-content-lg-center worklist">
+    <div className="row worklist justify-content-center">
       {items.map(({ work_item }, idx) => {
         if (!work_item) {
           return false;
         }
 
         const { data, uid } = work_item.document[0];
+
         return (
-          <div className="col-6 mb-5" key={`workItem__${idx}`}>
+          <div
+            className={`worklist-item worklist_${idx}`}
+            key={`workItem__${idx}`}
+          >
             <WorkItem data={data} uid={uid}></WorkItem>
           </div>
         );
